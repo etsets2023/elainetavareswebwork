5111704ecef0085974e0f940d4fc145847f5430d
ElaineTavares <etsets.tavares@gmail.com>
2024-02-13 16:53:08 -0300

    animacao js

--- js/script.js
+++ js/script.js
@@ -76,3 +76,20 @@ btnReact.addEventListener('click', () =>{
         tecnologiaReact.style.display = 'none'
     }
 })
+
+
+const myObserver = new IntersectionObserver ((entries) =>{
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting === true){
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
